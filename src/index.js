4e595ad77df56737649f02ac25a8fac9888fac2a
jessicago1010 <85047740+jessicago1010@users.noreply.github.com>
2021-06-14 23:43:40 -0400

    added API for the forecast data

--- src/index.js
+++ src/index.js
@@ -88,33 +88,42 @@ function displayCelsiusTemperature(event) {
   currentTemp.innerHTML = celsiusTemperature;
 }
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+return days[day];
+}
+
 function displayForecast(response) {
 let forecast = response.data.daily;
 let forecastElement = document.querySelector("#forecast");
 let days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]; 
 let forecastHTML = `<div class="row">`;
-forecast.forEach(function (forecastDay) {
+forecast.forEach(function (forecastDay, index) {
+  if (index < 6) {
   forecastHTML =
     forecastHTML +
     `<div class="col-2">
           <div class = "weather-forecast-date">
-          ${forecastDay.dt}
+          ${formatDay(forecastDay.dt)}
           </div>
             <img 
-          src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt="weather icon"
           id="icon"
           width="36"
           />
           <br />
           <span class="weather-forecast-temp-max">
-          18
+          ${Math.round(forecastDay.temp.max)}º
           </span>
           <span class="weather-forecast-temp-min">
-          12
+          ${Math.round(forecastDay.temp.min)}º
           </span>
         </div>
         `;
+        }
 });
 
 forecastHTML = forecastHTML + `</div>`;
